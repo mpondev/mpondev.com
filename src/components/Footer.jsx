5bf53e2c6da5229c0f69d44a398e6069400d4132
Mario Ponce <mario@mpondev.com>
2023-05-15 07:48:01 +0200

    Add Logo to Footer and some text to Home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import Logo from './Logo';
+import './Footer.scss';
+
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <div className="footer-left"></div>
+      <Logo sizeEm={1} />
+    </div>
+  );
 }
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import '../components/Header.scss';
+import './Home.scss';
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="intro">
+        <div className="intro-left">
+          <h2>Hola, mi nombre es</h2>
+          <h1>Mario Ponce</h1>
+        </div>
+        <div className="intro-right"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;

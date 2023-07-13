0b2c3d7416edadd5a61a71d5d61f65a29ed734c2
mpondev <mario@mpondev.com>
2023-07-13 12:42:56 +0200

    Tetris: add useGameOver hook to start the game

--- src/pages/Tetris/components/Game/Game.jsx
+++ src/pages/Tetris/components/Game/Game.jsx
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types';
+import { useGameOver } from '../../hooks/useGameOver';
 import styles from './Game.module.scss';
 
 function Game({ rows, columns }) {
+  const [gameOver, setGameOver, resetGameOver] = useGameOver();
+
   function start() {
-    console.log('start');
+    resetGameOver();
+    console.log(`Start gameOver is ${gameOver}`);
   }
 
   return (
